3fd80d3c4c337a0f304c07c2bcaf0f42a12a9a56
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-07-12 01:38:15 -0300

    basic get/post structure

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello World')
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+        return res
+            .setHeader('Content-type', 'aplication/json')
+            .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'john@example.com'
+        })
+
+        return res.end('Creating user')
+    }
+
+    return res.end('Criação de usuário!')
 })
 
-server.listen(3333)
+server.listen(3333) 
