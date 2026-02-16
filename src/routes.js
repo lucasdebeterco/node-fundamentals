059866b17159f877708c92c7c7d02ed6096bc085
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2026-02-16 13:35:42 -0300

    creating params regex

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const database = new Database()
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -15,7 +15,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -32,7 +32,7 @@ export const routes = [
   },
   {
     method: 'DELETE',
-    path: '/users/ID',
+    path: buildRoutePath('/users/:id'),
     handler: (req, res) => {
       return res.end()
     }
